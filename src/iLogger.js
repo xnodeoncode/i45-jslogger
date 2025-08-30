848042d1cf62505a8603e906a88332c679b421db
CIS Guru <cisguru@outlook.com>
2025-08-29 19:28:06 -0500

    See CHANGES.md for a list of changes.

--- src/iLogger.js
+++ src/iLogger.js
@@ -1,6 +1,17 @@
-export const iLogger = {
+/**
+ * @typedef {Object} iLogger
+ * @property {function(string):void} info
+ * @property {function(string):void} warn
+ * @property {function(string):void} error
+ * @property {function():Array} getEvents
+ */
+
+/** @type {iLogger} */
+const iLogger = {
   info: (message) => {},
   warn: (message) => {},
   error: (message) => {},
   getEvents: () => {},
 };
+
+export default iLogger;
